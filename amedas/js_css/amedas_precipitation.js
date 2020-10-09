3ebc6865994fa7df084ea81938a00f553ecc1132
renit <35866420+renit0424@users.noreply.github.com>
2020-10-09 10:09:15 +0900

    Update amedas_precipitation.js

--- amedas/js_css/amedas_precipitation.js
+++ amedas/js_css/amedas_precipitation.js
@@ -62,7 +62,7 @@ function init() {
         if (Number(json.stations[i].preall) != null) {
           precip_1h = Number(json.stations[i].preall.precip_1h);
           var updata = document.getElementById("updata");
-          updata.innerHTML += json.stations[0].updated.slice( 0, -5 ).replace( "-", "/").replace( "T", " ");
+          updata.innerHTML = json.stations[0].updated.slice( 0, -5 ).replace( "-", "/").replace( "T", " ");
           if (precip_1h >= 80.0) {
             var circle = L.circle([lat, lng], {
               radius: 2400,
