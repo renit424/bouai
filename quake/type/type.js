9fe3f6df8333a32a5419d09ff63dcd55c6b4c6b0
renit <35866420+renit0424@users.noreply.github.com>
2020-10-20 10:37:58 +0900

    Create type.js

---
+++ quake/type/type.js
@@ -0,0 +1,33 @@
+function init() {
+    const url = 'https://api.renitapps.com/quake.php';
+    //const url = 'https://bousai.renitapps.com/quake/test/20180906Hokkaido.json';
+    //const url = 'quake2.json';
+    const url2 = 'stations.json';
+    fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+        fetch(url2).then(function (response) {
+          return response.json();
+        }).then(function (json2) {
+          var issue = json[0].issue.type; 
+          if (issue == "ScalePrompt"){
+          }
+          else if (issue == "Destination"){
+          
+          }
+          else if (issue == "ScaleAndDestination"){
+          
+          }
+          else if (issue == "DetailScale"){
+          
+          }
+          else if (issue == "Foreign"){
+          
+          }
+          else if (issue == "Other"){
+          
+          }
+          else{}
+       });
+     });
+  }
