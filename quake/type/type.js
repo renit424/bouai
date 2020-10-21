1bd8c6b4d8ccde1d5f6436bb2b9e1ae66f205e9c
renit <35866420+renit0424@users.noreply.github.com>
2020-10-21 10:52:18 +0900

    Update type.js

--- quake/type/type.js
+++ quake/type/type.js
@@ -11,18 +11,19 @@ function init() {
         }).then(function (json2) {
           var issue = json[0].issue.type; 
           if (issue == "ScalePrompt"){
+	     appendScript("/quake/type/ScalePrompt.js");
           }
           else if (issue == "Destination"){
-          
+             appendScript("/quake/type/Destination.js");
           }
           else if (issue == "ScaleAndDestination"){
-          
+             appendScript("/quake/type/ScaleAndDestination.js");
           }
           else if (issue == "DetailScale"){
-          
+             appendScript("/quake/type/DetailScale.js");
           }
           else if (issue == "Foreign"){
-            appendScript("/quake/type/Foreign.js");
+             appendScript("/quake/type/Foreign.js");
           }
           else if (issue == "Other"){
           
